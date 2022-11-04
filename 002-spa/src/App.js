e2331f67352c20fdcc9c4bc033569e9a4c31a0d8
Salama Ashoush <salamaashoush@gmail.com>
2022-11-04 15:43:00 +0100

    wip 10

--- 002-spa/src/App.js
+++ 002-spa/src/App.js
@@ -1,13 +1,21 @@
-import React, { Component } from "react"
+import React, { Component, lazy, Suspense } from "react"
 import { Route, Switch } from "react-router"
 import AuthWrapper from "./components/AuthWrapper"
-import OauthRedirect from "./components/OauthRedirect"
+const OauthRedirect = lazy(() => import("./components/OauthRedirect"))
 
 class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/redirect" render={() => <OauthRedirect />} />
+        <Route
+          exact
+          path="/redirect"
+          render={() => (
+            <Suspense>
+              <OauthRedirect />
+            </Suspense>
+          )}
+        />
         <Route>
           <AuthWrapper />
         </Route>
